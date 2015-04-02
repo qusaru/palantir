f8c152db8b544a41e8a3137feda8029dcbb22bad
qusaru <qusaru@gmail.com>
2015-04-02 11:33:57 +0400

    First popup page
    Add textarea

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-popup.js
+var window = window;
+
+var textarea = window.document.createElement('textarea');
+	textarea.className = 'input';
+
+	window.document.body.appendChild(textarea, window.document.body);
